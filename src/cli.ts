cf39a211b59b39c1148a53fa164b923db26d45f3
Sebastian Roming <sebastian@webmonkey.io>
2024-02-22 12:01:45 +0100

    updated usage

--- src/cli.ts
+++ src/cli.ts
@@ -11,28 +11,28 @@ const outputFilename  = argv['out-file'] || 'license-output.md';
 const _isTable         = argv['table'] || false;
 const _depth           = argv['depth'] || 1;
 const _devDependencies = argv['with-dev-dependencies'] || false;
+const _templateDir     = argv['template-dir'] || `${process.cwd()}/assets/`;
+const _templateFile    = argv['template-file'] || 'template.txt';
 
-interface ICliOptions {
+interface CliOptions {
   template?: string
   depth?: number,
   useDevDependencies?: boolean
 };
 
-let options: ICliOptions = {
+let options: CliOptions = {
   depth: _depth,
   useDevDependencies: _devDependencies
 };
 
-if (argv['template-file']) {
-  const templateDirectory = argv['template-dir'] || `${process.cwd()}/assets/`;
-  const templateFilename  = argv['template-file'] || 'template.txt';
-  const templateContents  = cliFs.readFileSync(cliPath.resolve(templateDirectory, templateFilename), 'utf8');
+if (argv['template-file'] || argv['template-dir']) {
+  const templateContents  = cliFs.readFileSync(cliPath.resolve(_templateDir, _templateFile), 'utf8');
   options.template        = templateContents;
 }
 
 if (_isTable) {
-  cliLicenser.reporter.table().generate(options).then((rep: Report) => rep.write(cliPath.resolve(outputDirectory, outputFilename)));
+  cliLicenser.reporter.table().generate(options).then((rep: LicenseReport) => rep.write(cliPath.resolve(outputDirectory, outputFilename)));
 } else {
-  cliLicenser.reporter.generate(options).then((rep: Report) => rep.write(cliPath.resolve(outputDirectory, outputFilename)));
+  cliLicenser.reporter.generate(options).then((rep: LicenseReport) => rep.write(cliPath.resolve(outputDirectory, outputFilename)));
 }
 
